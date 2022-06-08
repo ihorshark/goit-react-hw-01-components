c34d017ebb315c525d45a62c01ab4bbfa4e1ec8e
ihorshark <igor.sharkadi.93@gmail.com>
2022-06-08 17:04:17 +0300

    Task3 demo

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -20,16 +20,3 @@ export const App = () => {
     </div>
   );
 };
-
-{
-  /* <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    ></div> */
-}
